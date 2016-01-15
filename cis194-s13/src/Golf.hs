65c190ed800ffaa0200e3d59e5812220870899a2
Prat <banqfirn@gmail.com>
2016-01-14 22:41:17 -0500

    Add localMaxima

--- cis194-s13/src/Golf.hs
+++ cis194-s13/src/Golf.hs
@@ -21,3 +21,23 @@ skips ls = map (map snd) (map filterByIndex (zipWithIndex(map zipWithIndex (repl
                 divisible :: Int -> Int -> Bool
                 divisible 1 _ = True
                 divisible n m = m `mod` n == 0
+
+-- pointfree version
+-- skips' = map (map snd) . map filterByIndex . zipWithIndex . map zipWithIndex . (replicate =<< length)
+
+-- Exercise 2 Local maxima
+-- A local maximum of a list is an element of the list which is strictly
+-- greater than both the elements immediately before and after it.
+--     localMaxima [2,9,5,6,1] == [9,6]
+--     localMaxima [2,3,4,1,5] == [4]
+--     localMaxima [1,2,3,4,5] == []
+localMaxima :: [Integer] -> [Integer]
+localMaxima = concat . map middleMax . group3
+                where
+                    middleMax :: [Integer] -> [Integer]
+                    middleMax (a:b:[c]) = if b > a && b > c then [b] else []
+                    middleMax _         = []
+                    group3 :: [t] -> [[t]]
+                    group3 ls = if length ls < 3
+                                then []
+                                else take 3 ls : group3 (tail ls)
