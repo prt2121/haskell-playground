b273747ba51a45340c69af201af56abfe2ee5567
Prat <banqfirn@gmail.com>
2016-01-16 12:52:39 -0500

    Add Wholemeal fun2

--- cis194-s13/src/HigherOrder04.hs
+++ cis194-s13/src/HigherOrder04.hs
@@ -12,3 +12,13 @@ fun1 (x:xs)
 
 fun1' :: [Integer] -> Integer
 fun1' = foldr (\x acc -> (x - 2) * acc) 1 . filter even
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n  | even n = n + fun2 (n `div` 2)
+        | otherwise = fun2 (3 * n + 1)
+
+fun2' :: Integer -> Integer
+fun2' = sum . filter even . takeWhile (1 /=) . iterate f
+            where f x | even x    = x `div` 2
+                      | otherwise = 3 * x + 1
