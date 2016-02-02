cacc4010c76be3e617096952e2e74fb2d4d396b5
Prat <banqfirn@gmail.com>
2016-02-01 22:58:56 -0500

    Add cis194 hw05 ex02

--- cis194-s13-hw05/src/Main.hs
+++ cis194-s13-hw05/src/Main.hs
@@ -3,12 +3,17 @@
 module Main where
 
 import ExprT
+import Parser
 
 -- Exercise 1
 eval :: ExprT -> Integer
 eval (Lit n)      = n
 eval (Add e1 e2)  = eval e1 + eval e2
 eval (Mul e1 e2)  = eval e1 * eval e2
+
+-- Exercise 2
+evalStr :: String -> Maybe Integer
+evalStr = (eval <$>) . (parseExp Lit Add Mul)
 
 main :: IO ()
 main = do
