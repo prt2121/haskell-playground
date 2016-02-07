43fea2cb6304f281b9f8de0cccabb4e2c3a7c7c0
Prat <banqfirn@gmail.com>
2016-02-07 12:02:32 -0500

    Complete hw 5 ex 4

--- cis194-s13-hw05/src/Main.hs
+++ cis194-s13-hw05/src/Main.hs
@@ -29,6 +29,41 @@ instance Expr ExprT where
 reify :: ExprT -> ExprT
 reify = id
 
+-- Exercise 4
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit = (>0)
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit x                     = MinMax x
+  add (MinMax x) (MinMax y) = MinMax $ max x y
+  mul (MinMax x) (MinMax y) = MinMax $ min x y
+
+instance Expr Mod7 where
+  lit x                     = Mod7 $ mod x 7
+  add (Mod7 x) (Mod7 y)     = Mod7 $ mod (x+y) 7
+  mul (Mod7 x) (Mod7 y)     = Mod7 $ mod (x*y) 7
+
+testExp :: Expr a => Maybe a
+testExp     = parseExp lit add mul "(3 * -4) + 5"
+testInteger :: Maybe Integer
+testInteger = testExp :: Maybe Integer
+testBool :: Maybe Bool
+testBool    = testExp :: Maybe Bool
+testMM :: Maybe MinMax
+testMM      = testExp :: Maybe MinMax
+testSat :: Maybe Mod7
+testSat     = testExp :: Maybe Mod7
+
 main :: IO ()
 main = do
   putStrLn "hello world"
