2339135858a07c5f1d278a5ad27f0af34cd2ea54
Prat <banqfirn@gmail.com>
2016-02-14 21:33:25 -0500

    Add cs194 hw06 laziness ex4~5

--- cis194-s13-hw06/src/Hw06Lazy.hs
+++ cis194-s13-hw06/src/Hw06Lazy.hs
@@ -24,6 +24,26 @@ streamToList (Cons n s) = n : streamToList s
 instance Show a => Show (Stream a) where
     show = show . take 20 . streamToList
 
+-- Exercise 4
+-- generates a stream containing infinitely many copies of the
+-- given element.
+streamRepeat :: a -> Stream a
+streamRepeat n = Cons n $ streamRepeat n
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons n s) = Cons (f n) $ streamMap f s
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f i = Cons i $ streamFromSeed f $ f i
+
+-- Exercise 5
+nats :: Stream Integer
+nats = streamFromSeed (+1) 0
+
+--ruler :: Stream Integer
+-- 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0, 4,...
+-- todo :P
+
 main :: IO ()
 main = do
   putStrLn "hello world"
