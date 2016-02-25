a3cc5778d2325abf7fb23a4947323c8d0c8d4b82
Prat <banqfirn@gmail.com>
2016-02-24 21:08:05 -0500

    Pointfreeify. Update folder

--- exercism/sum-of-multiples/SumOfMultiples.hs
+++ exercism/sum-of-multiples/SumOfMultiples.hs
@@ -1,7 +1,7 @@
 module SumOfMultiples where
 
 sumOfMultiples :: [Int] -> Int -> Int
-sumOfMultiples ls m = sum [n | n <- [1..(m - 1)], any (\x -> n `mod` x == 0) ls]
+sumOfMultiples ls m = sum [n | n <- [1..(m - 1)], any ((0 ==) . (n `mod`)) ls]
 
 sumOfMultiplesDefault :: Int -> Int
 sumOfMultiplesDefault m = sumOfMultiples [3,5] m
