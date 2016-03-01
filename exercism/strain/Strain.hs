37a522a10b65dfb7dd9ebfe8328d7acd25baa875
Prat <banqfirn@gmail.com>
2016-02-29 20:58:32 -0500

    Avoid std lib

--- exercism/strain/Strain.hs
+++ exercism/strain/Strain.hs
@@ -1,7 +1,7 @@
 module Strain where
 
 keep :: (a -> Bool) -> [a] -> [a]
-keep = filter
+keep p ls = foldr (\x acc -> if p x then x : acc else acc) [] ls
 
 discard :: (a -> Bool) -> [a] -> [a]
 discard = filter . (not .)
