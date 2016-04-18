1e284fcdea4d11d82af0c4c52ef4b99e1580a53e
Prat <banqfirn@gmail.com>
2016-04-17 20:19:46 -0400

    add Type-Kwon-Do ex c6p216

--- haskell-book/Chapter06.hs
+++ haskell-book/Chapter06.hs
@@ -67,3 +67,12 @@ instance Eq a => Eq (Which a) where
 instance (Eq a, Eq b) => Eq (EitherOr a b) where
   (==) (Hello x) (Hello x') = x == x'
   (==) (Goodbye y) (Goodbye y') = y == y'
+
+-- Type-Kwon-Do
+chk :: Eq b => (a -> b) -> a -> b -> Bool
+chk f a b = f a == b
+
+-- Hint: use some arithmetic operation to
+-- combine values of type 'b'. Pick one.
+arith :: Num b => (a -> b) -> Integer -> a -> b
+arith f n a = f a + fromInteger n
