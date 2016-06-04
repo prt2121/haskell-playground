9fa4ea1941c75f0ccbd6568e1bd0b628d92de37b
Prat <banqfirn@gmail.com>
2016-06-04 11:21:05 -0400

    add ugly userLogin

--- transformer/src/Main.hs
+++ transformer/src/Main.hs
@@ -8,6 +8,8 @@ import Data.Map as Map
 import Control.Applicative
 
 data LoginError = InvalidEmail
+                | NoSuchUser
+                | WrongPassword
   deriving Show
 
 getDomain :: Text -> Either LoginError Text
@@ -16,10 +18,47 @@ getDomain email =
     [name, domain] -> Right domain
     _              -> Left InvalidEmail
 
+printResult :: Either LoginError Text -> IO ()
+printResult domain =
+  case domain of
+    Right text        -> T.putStrLn (append "Domain: " text)
+    Left InvalidEmail -> T.putStrLn "ERROR: Invalid domain"
+
+-- *Main> getDomainIO
+-- Enter email address:
+-- b@gmail.com
+-- Right "gmail.com"
+
+getDomainIO :: IO (Either LoginError Text)
+getDomainIO = do
+  T.putStrLn "Enter email address:"
+  email <- T.getLine
+  return (getDomain email)
+
+users :: Map Text Text
+users = Map.fromList [("test.com", "test1234"), ("localhost", "password")]
+
+userLogin :: IO (Either LoginError Text)
+userLogin = do
+  d <- getDomainIO
+  case d of
+    Right domain ->
+      case Map.lookup domain users of
+        Just p -> do
+          T.putStrLn "Enter password:"
+          password <- T.getLine
+          if p == password
+             then return d
+             else return (Left WrongPassword)
+        Nothing -> return (Left NoSuchUser)
+    left -> return left
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  T.putStrLn "hello world"
 
 -- *Main> :set -XOverloadedStrings
 -- *Main> getDomain "test@example.com"
 -- Right "example.com"
+-- *Main> printResult $ getDomain "test@example.com"
+-- Domain: example.com
