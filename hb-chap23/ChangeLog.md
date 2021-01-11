8503113c52bf5d5da1b05bafd0a1f127d7bc774f
Prat T <pt2121@users.noreply.github.com>
2021-01-10 19:10:22 -0800

    redo haskell book chapter 23

---
+++ hb-chap23/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for hb-chap23
+
+## Unreleased changes
---
+++ hb-chap23/src/Lib.hs
@@ -0,0 +1,36 @@
+module Lib
+    ( roll
+    ) where
+
+import Control.Monad.Trans.State
+import System.Random
+
+roll :: IO ()
+roll = do
+  gen <- newStdGen
+  let (d, g) = runState rollDie gen
+  print d
+
+data Die = DieOne
+  | DieTwo
+  | DieThree
+  | DieFour
+  | DieFive
+  | DieSix
+  deriving (Eq, Show)
+
+intToDie :: Int -> Die
+intToDie n =
+  case n of
+  1 -> DieOne
+  2 -> DieTwo
+  3 -> DieThree
+  4 -> DieFour
+  5 -> DieFive
+  6 -> DieSix
+  x -> error $ "intToDie got non 1-6 integer: " ++ show x
+
+rollDie :: State StdGen Die
+rollDie = state $ do
+  (n, s) <- randomR (1, 6)
+  return (intToDie n, s)
